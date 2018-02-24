3d6a2010872d0ccfece3e775eb699c5971d39d39
Shiv <shivgunter@gmail.com>
2018-02-24 21:41:51 +0800

    checkbox done

--- src/routes/devices/list/components/device/index.js
+++ src/routes/devices/list/components/device/index.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react'
 import './index.css'
 
 class Device extends Component {
+  constructor(props) {
+    super(props);
+    const { device } = this.props
+    this.state = {checkboxState: device.value};
+  }
+
+  toggle(event) {
+   this.setState({checkboxState: !this.state.checkboxState});
+   
+   // POST action
+   // send as application/json {device_id :: string, action :: string} action is one of {'on', 'off'}
+  }
+
   render(){
     const { device } = this.props
     return (
       <div className="device">
         <h3>{device.name}</h3>
-        <input type="checkbox" checked={device.value}/>
+        <input type="checkbox" defaultChecked={device.value} onClick={this.toggle.bind(this)}/>
       </div>
     )
   }
