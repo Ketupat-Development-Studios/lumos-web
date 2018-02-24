52f7e00d28f778fec7f7113d1d388fd33d6f7083
Huey <leejinhuey@gmail.com>
2018-02-24 23:26:04 +0800

    show route

---
+++ src/routes/spells/show/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './index.css'
+
+class SpellDetailScren extends Component {
+  constructor(){
+    super()
+  }
+  render(){
+    return (
+      <div className="spell-detail">
+
+      </div>
+    )
+  }
+}
